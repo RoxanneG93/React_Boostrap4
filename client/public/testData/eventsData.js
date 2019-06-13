16e4ac8bd0082f6c7df5e6e8e1aa400dac597e9f
RoxanneG93 <rxrblog@outlook.com>
2019-06-13 09:58:45 -0500

    changes

---
+++ client/public/testData/eventsData.js
@@ -0,0 +1,9 @@
+import uuid from 'react-uuid';
+export const event = {
+    id: uuid(),
+    name: "Awesome Event",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatibus facilis, quidem obcaecati minus tempora maxime cumque est odio modi impedit voluptates culpa dolore eligendi iste voluptatem laboriosam, distinctio ea!",
+    dates: "2019-01-05 to 2019-01-05",
+    image: "25f39a88-459b-11e9-ba7a-81151779d1a9-476313-pgaboq-187.jpg"
+
+}
---
+++ client/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    I would be part of the homepage.
+                </div>
+            </div>
+            
+        </div>
+    )
+}
